7827a95ee5976beb0aa5e28bb5da4b50d20806ca
Jamilly Vitorya <jamillyvitorya14@gmail.com>
2022-06-11 00:18:08 -0300

    test

---
+++ CustoFinalCarro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    float preco_fab, p_dist, p_imp, z;
+
+    scanf("%f %f %f", &preco_fab, &p_dist, &p_imp);
+    
+    z = 
+    scanfss
+    
+
+    printf("O VALOR DO CARRO E = %.2f\n", z);
+
+    return 0;
+}
