1eff311da8ba2d0419225f4f71544a4eb3808865
jamilly <jamillyvitorya14@gmail.com>
2022-06-16 21:40:28 -0300

    testagem

--- Arredondamento.c
+++ Arredondamento.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    float n;
+    float um, dois, tres;
 
-    int n, a, cem, cinq, dez, um;
-    double r, i;
-
-    scanf("%d", &n);
-
-    for(i=0; i<n; i++){
-        a++;
-        n/=100;
-    } 
-    printf("%d\n", a);
-    // printf("NOTAS DE 100 = %lf\n", cem);
-    // printf("NOTAS DE 50 = %lf\n", cinq);
-    // printf("NOTAS DE 10 = %lf\n", dez);
-    // printf("NOTAS DE 1 = %lf\n", um);
+    scanf("%f", &n);
     
+    um = (n/10);
+   
+    printf("%f\n", um);
+    // printf("%f\n", dois);
+    // printf("%f\n", tres);
+
     return 0;
 }
